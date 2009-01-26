36d499e304b7f584054f4c61cc3eee2186113d07
Andrey Smirnov <Smirnov.Andrey@gmail.com>
2009-01-26 15:35:10 +0300

    Ссылка на лицензию GPLv3.

--- spamfighter/api/message/test/test_log.py
+++ spamfighter/api/message/test/test_log.py
@@ -2,7 +2,21 @@
 #
 # SpamFighter, Copyright 2008 NetStream LLC (http://netstream.ru/, we@netstream.ru)
 #
-# $Id$
+# This file is part of SpamFighter.
+#
+# SpamFighter is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SpamFighter is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SpamFighter.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 """
 Тесты на L{spamfighter.api.message.log}.
